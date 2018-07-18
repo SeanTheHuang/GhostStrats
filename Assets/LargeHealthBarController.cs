daf7cfe9f6db0ef682e64eebf5730e9035dcd342
prunefriend <jwjmair@gmail.com>
2018-07-18 17:11:02 +1200

    Main health bar moves with little health bar

---
+++ Assets/LargeHealthBarController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LargeHealthBarController : MonoBehaviour
+{
+    private Color m_highHealth;
+    private Color m_midHealth;
+    private Color m_lowHealth;
+
+    private RectTransform healthBarRect;
+    private Image m_HealthBarImage;
+
+    private float m_healthBarMaxWidth; // The width of the health bar when at 100%
+    private float m_healthBarCurrentWidth; // The current width of the healthbar
+
+    private float m_maxHealth; // The max health of the ghost
+    private float m_currentHealth; // The current health of the ghost
+    private float m_currentHealthBar; // The current health the bar is set to
+    private float m_healthToBarWidth; // The constant that converts the current health to bar length
+
+    public void Initalize(float maxHealth, Color highHealth, Color midHealth, Color lowHealth)
+    {
+        m_highHealth = highHealth;
+        m_midHealth = midHealth;
+        m_lowHealth = lowHealth;
+
+        m_maxHealth = maxHealth;
+        m_currentHealth = m_maxHealth;
+        m_currentHealthBar = m_maxHealth;
+
+        m_healthBarMaxWidth = GetComponent<RectTransform>().rect.width;
+        m_HealthBarImage = GetComponent<Image>();
+        healthBarRect = GetComponent<RectTransform>();
+        m_healthToBarWidth = GetComponent<RectTransform>().rect.width / m_maxHealth;
+    }
+
+    // Update the ghosts health bar
+    public void UpdateHealthBar(float newHealth)
+    {
+        m_currentHealth = newHealth;
+        StartCoroutine(LerpHealthBar(1.5f));
+        StartCoroutine(LerpHealthBarColor(1.5f));
+    }
+
+    private IEnumerator LerpHealthBarColor(float time)
+    {
+        // The health bars are currently in the same colour zone. No need to lerp colour
+        if ((m_currentHealth > m_maxHealth * 0.5f && m_currentHealthBar > m_maxHealth * 0.5f)
+        || ((m_currentHealth > m_maxHealth * 0.25f && m_currentHealthBar > m_maxHealth * 0.25f) && (m_currentHealth <= m_maxHealth * 0.5f && m_currentHealthBar <= m_maxHealth * 0.5f))
+        || (m_currentHealth <= m_maxHealth * 0.25f && m_currentHealthBar <= m_maxHealth * 0.25f))
+            yield return null;
+
+        Color newBarColor;
+        Color oldBarColor = m_HealthBarImage.color;
+        if (m_currentHealth > m_maxHealth * 0.5f)
+            newBarColor = m_highHealth;
+        else if (m_currentHealth > m_maxHealth * 0.25f)
+            newBarColor = m_midHealth;
+        else
+            newBarColor = m_lowHealth;
+
+        float elapsedTime = 0;
+
+        while (elapsedTime < time)
+        {
+            Color lerpColor = Color.Lerp(oldBarColor, newBarColor, (elapsedTime / time));
+            m_HealthBarImage.color = lerpColor;
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private IEnumerator LerpHealthBar(float time)
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < time)
+        {
+            float width = Mathf.Lerp(m_currentHealthBar, m_currentHealth, (elapsedTime / time)) * m_healthToBarWidth;
+            transform.position = new Vector2(transform.position.x - ((healthBarRect.rect.width - width) / 2), transform.position.y);
+            healthBarRect.sizeDelta = new Vector2(width, healthBarRect.rect.height);
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GhostPortraitController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GhostPortraitController : MonoBehaviour {
+
+    public Color m_highHealth;
+    public Color m_midHealth;
+    public Color m_lowHealth;
+
+    public GameObject m_ghost;
+    public GameObject m_healthBar;
+    public GameObject m_largeHealthBar;
+    private LargeHealthBarController largeHealthBarController;
+
+    private RectTransform healthBarRect;
+    private Image m_HealthBarImage;
+
+    private float m_healthBarMaxWidth; // The width of the health bar when at 100%
+    private float m_healthBarCurrentWidth; // The current width of the healthbar
+
+    private float m_maxHealth; // The max health of the ghost
+    private float m_currentHealth; // The current health of the ghost
+    private float m_currentHealthBar; // The current health the bar is set to
+    private float m_healthToBarWidth; // The constant that converts the current health to bar length
+
+    // Use this for initialization
+    void Start() {
+        m_maxHealth = m_ghost.GetComponent<GhostController>().m_maxHealth;
+        m_currentHealth = m_maxHealth;
+        m_currentHealthBar = m_maxHealth;
+
+        m_healthBarMaxWidth = m_healthBar.GetComponent<RectTransform>().rect.width;
+        m_HealthBarImage = m_healthBar.GetComponent<Image>();
+        healthBarRect = m_healthBar.GetComponent<RectTransform>();
+        m_healthToBarWidth = m_healthBar.GetComponent<RectTransform>().rect.width / m_maxHealth;
+
+        largeHealthBarController = m_largeHealthBar.GetComponent<LargeHealthBarController>();
+        largeHealthBarController.Initalize(m_maxHealth, m_highHealth, m_midHealth, m_lowHealth);
+
+        m_currentHealth /= 2;
+    }
+
+    private void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            UpdateHealthBar();
+        }
+    }
+
+    // Update the ghosts health bar
+    public void UpdateHealthBar()
+    {
+        //m_currentHealth = m_ghost.GetComponent<GhostController>().GetCurrentHealth();
+        StartCoroutine(LerpHealthBar(1.5f));
+        StartCoroutine(LerpHealthBarColor(1.5f));
+
+        largeHealthBarController.UpdateHealthBar(m_currentHealth);
+    }
+
+    private IEnumerator LerpHealthBarColor(float time)
+    {
+        // The health bars are currently in the same colour zone. No need to lerp colour
+        if ((m_currentHealth > m_maxHealth * 0.5f && m_currentHealthBar > m_maxHealth * 0.5f)
+        || ((m_currentHealth > m_maxHealth * 0.25f && m_currentHealthBar > m_maxHealth * 0.25f) && (m_currentHealth <= m_maxHealth * 0.5f && m_currentHealthBar <= m_maxHealth * 0.5f))
+        || (m_currentHealth <= m_maxHealth * 0.25f && m_currentHealthBar <= m_maxHealth * 0.25f))
+            yield return null;
+
+        Color newBarColor;
+        Color oldBarColor = m_HealthBarImage.color;
+        if (m_currentHealth > m_maxHealth * 0.5f)
+            newBarColor = m_highHealth;
+        else if (m_currentHealth > m_maxHealth * 0.25f)
+            newBarColor = m_midHealth;
+        else
+            newBarColor = m_lowHealth;
+
+        float elapsedTime = 0;
+
+        while (elapsedTime < time)
+        {
+            Color lerpColor = Color.Lerp(oldBarColor, newBarColor, (elapsedTime / time));
+            m_HealthBarImage.color = lerpColor;
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private IEnumerator LerpHealthBar(float time)
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < time)
+        {
+            float width = Mathf.Lerp(m_currentHealthBar, m_currentHealth, (elapsedTime / time)) * m_healthToBarWidth;
+            m_healthBar.transform.position = new Vector2(m_healthBar.transform.position.x - ((healthBarRect.rect.width - width) / 2), m_healthBar.transform.position.y);
+            healthBarRect.sizeDelta = new Vector2(width, healthBarRect.rect.height);
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
