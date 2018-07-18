d8017549d9eefe4ba41625f1494a0d66e679b03e
HugoAdams <hugogfadams@gmail.com>
2018-07-18 16:15:44 +1200

    change entity base and some punk controls

--- Assets/Scripts/AI/CharacterControllers/PunkController.cs
+++ Assets/Scripts/AI/CharacterControllers/PunkController.cs
@@ -8,6 +8,7 @@ public class PunkController : EntityBase
     float circleSightRadius = 3;
     float forwardSightAngle = 30;
     float forwardSightRadius = 6;
+    List<Collider> m_Targets = new List<Collider> { };
 
     public override void OnDeath()
     {
@@ -27,34 +28,53 @@ public class PunkController : EntityBase
 
     void Sight()
     {
+        m_Targets.Clear();
         Collider[] targets = Physics.OverlapSphere(transform.position, circleSightRadius, m_targetmask);
-        List<Collider> fTargets = new List<Collider> { };
         if (targets.Length != 0)
         {
-            //a target is in the sight range
-            if(targets.Length >= 2)
-            {//multiple target choose best
-                //check health, then check distance
+            for(int i=0; i < targets.Length;i++)
+            {
+                m_Targets.Add(targets[i]);
             }
-
-            
         }
-
+        //need to raycast for walls
         targets = Physics.OverlapSphere(transform.position, forwardSightRadius, m_targetmask);
-        for (int i = 0; i < targets.Length; i++) 
+        for (int i = 0; i < targets.Length; i++)
         {
             Vector2 v1 = new Vector2(transform.forward.x, transform.forward.z);
             Vector2 v2 = new Vector2(targets[i].transform.position.x, targets[i].transform.position.z);
-            if(Vector2.Angle(v1, v2) < forwardSightAngle)
+            if (Vector2.Angle(v1, v2) < forwardSightAngle)
             {
-                fTargets.Add(targets[i]);
+                m_Targets.Add(targets[i]);
             }
         }
+    }
 
-        if (fTargets.Count != 0)
+    void ChooseTarget()
+    {
+        if(m_Targets.Count == 0)
         {
-            //target is forward
+            return;
         }
-        
+
+        int lowestHealth = 10000;
+        for (int i = 0; i < m_Targets.Count; i++) 
+        {
+            Transform t = m_Targets[i].transform;
+            
+
+            if(t.GetComponent<GhostController>())
+            {//ghosts
+                if (t.GetComponent<GhostController>().GetCurrentHealth() < lowestHealth)
+                {//check for health
+                    lowestHealth = t.GetComponent<GhostController>().GetCurrentHealth();
+                }
+            }
+            //ghosts
+            //check if they are in range, then check lowest health, then go for closest
+
+            
+            //then targets
+        }
     }
 }
