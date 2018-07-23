4a35a4680a15c3e198fc6aa32303dac9c712c458
MDS\sean.hua7281 <>
2018-07-24 10:59:45 +1200

    Started title animation between rounds

---
+++ Assets/Scripts/ScriptableObject/EffectTextTemplate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Text/EffectTextTemplate")]
+public class EffectTextTemplate : ScriptableObject {
+
+    public string m_text = "<sprite=1> SAMPLE TEXT!!";
+    public Color m_baseColor = Color.white;
+    public Color m_outlineColor = Color.white;
+}
