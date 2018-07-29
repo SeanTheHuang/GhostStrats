525c6b637c2f72e4a0bd607e1d94d2d5e930d32b
MDS\sean.hua7281 <>
2018-07-30 10:24:44 +1200

    started punk spawners #2

--- Assets/Scripts/PunkSpawner.cs
+++ Assets/Scripts/PunkSpawner.cs
@@ -11,4 +11,48 @@ public struct SpawnLogic
 
 public class PunkSpawner : MonoBehaviour {
 
+    public SpawnLogic[] m_spawns;
+    public PunkSpawnPosition[] m_spawnPoints;
+    public Transform m_punkPrefab;
+
+    public bool m_running
+    { get; private set; }
+
+    int m_turnCounter = 0;
+    int m_spawnLogicIndex = 0;
+
+    public void PlayTurn()
+    {
+        if (m_spawnLogicIndex >= m_spawns.Length) // No more punks to spawn!!
+            return;
+
+        m_turnCounter++;
+
+        if (m_turnCounter >= m_spawns[m_spawnLogicIndex].m_numTurnsUntilSpawn)
+        {
+            m_running = true;
+            StartCoroutine(SpawnAnimation());
+        }
+    }
+
+    IEnumerator SpawnAnimation()
+    {
+        for (int i = 0; i < m_spawns[m_spawnLogicIndex].m_numPunksToSpawn; i++)
+        {
+            int randSpawnPoint = Random.Range(0, m_spawnPoints.Length);
+            Quaternion rotation = Quaternion.AngleAxis((float)m_spawnPoints[randSpawnPoint].m_startDirection, Vector3.up);
+            Transform temp = Instantiate(m_punkPrefab, m_spawnPoints[randSpawnPoint].transform.position, rotation);
+            CameraControl.Instance.SetFollowMode(temp); // Look at dat new punk
+            // TODO, ADD PUNK TO PUNKLIST IN GAMEMASTER
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        m_spawnLogicIndex++; // Next spawn wave
+        m_turnCounter = 0;
+        m_running = false;
+        yield return null;
+    }
+
+    public bool SpawnerFinished()
+    { return m_spawnLogicIndex >= m_spawns.Length; }
 }
