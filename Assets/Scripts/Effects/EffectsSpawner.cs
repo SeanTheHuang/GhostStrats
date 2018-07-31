5017427ac3cbd5643014159e0fdd141115b61f62
prunefriend <jwjmair@gmail.com>
2018-07-31 15:01:34 +1200

    Added a repsawn particle on spawn

--- Assets/Scripts/Effects/EffectsSpawner.cs
+++ Assets/Scripts/Effects/EffectsSpawner.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class EffectsSpawner : MonoBehaviour {
 
     public Transform m_poofParticlesPrefab;
+    public Transform m_respawnParticlesPrefab;
 
     public void SpawnPoofPrefab(Vector3 _location)
     {
         Instantiate(m_poofParticlesPrefab, _location, m_poofParticlesPrefab.rotation);
+    }
+
+    public void SpawnRespawnPoofPrefab(Vector3 _location)
+    {
+        Instantiate(m_respawnParticlesPrefab, _location, m_respawnParticlesPrefab.rotation);
     }
 }
