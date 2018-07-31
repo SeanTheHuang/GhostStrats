bd5b1f578779df497dc134776f6fa3aef3e24a07
MDS\hugo.ada7271 <hugogfadams@gmail.com>
2018-07-31 15:50:43 +1200

    health bars are definitly clamped

--- Assets/Scripts/PunkHealthBar.cs
+++ Assets/Scripts/PunkHealthBar.cs
@@ -27,17 +27,21 @@ public class PunkHealthBar : MonoBehaviour
         transform.rotation = Quaternion.identity;
         if(m_hp != m_punkCon.GetCurrentHealth())
         {//change health
+            float scale;
             if (m_hp < 0)
             {
                 m_hp = 0;
+                scale = 0;
             }
-            Debug.Log("TIME TO CHANGE THE HELATH");
-            m_hp = m_punkCon.GetCurrentHealth();
-            float scale = (float)m_hp / (float)m_maxHp;
-            Debug.Log("greenbar xscale should be " + scale);
-            m_greenBar.localScale = new Vector3(scale, 1, 1);
-            Debug.Log("greenbar xscale is " + m_greenBar.localScale.x);
+            else
+            {
+                m_hp = m_punkCon.GetCurrentHealth();
+                 scale = (float)m_hp / (float)m_maxHp;
 
+            }
+            scale = Mathf.Clamp01(scale);
+            m_greenBar.localScale = new Vector3(scale, 1, 1);
+            
             float xoff = 1 - scale;
             xoff *= 0.5f;
             m_greenBar.localPosition = new Vector3(-xoff, 0, -0.001f);
