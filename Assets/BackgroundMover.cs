495aa66bcdd83ccec1eaf520b9760935aac74c24
MDS\sean.hua7281 <>
2018-08-01 13:46:00 +1200

    title screen shit

--- Assets/BackgroundMover.cs
+++ Assets/BackgroundMover.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class BackgroundMover : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float m_maxMoveAmount = 50;
+    public float m_newTargetTime = 0.3f;
+    public float m_lerpVal = 10;
+    Vector3 m_startPos, m_targetPos;
+    Vector3 m_startScale;
+    float m_lastTime;
+
+    private void Awake()
+    {
+        m_targetPos = m_startPos = transform.position;
+        m_startScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        if (Time.time - m_lastTime > m_newTargetTime)
+        {
+            m_lastTime = Time.time;
+            m_targetPos = new Vector3(Random.Range(-m_maxMoveAmount, m_maxMoveAmount), Random.Range(-m_maxMoveAmount, m_maxMoveAmount), 0) + m_startPos;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, m_targetPos, Time.deltaTime * m_lerpVal);
+        float val = (Mathf.Sin(Time.time * 0.1f) + 1) * 0.1f;
+        transform.localScale = m_startScale + new Vector3(val, val, val);
+    }
 }
